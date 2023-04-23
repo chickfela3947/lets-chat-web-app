32392b2ec7cda40db8a5d6b9f1000bf1a3f0bd37
chickfela3947 <124513798+chickfela3947@users.noreply.github.com>
2023-04-23 10:43:03 -0400

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCLpd9jnvYVAY8RmmW7lHVJ2seeULyWtnY",
+    authDomain: "lets-chat-web-app-2-31cbe.firebaseapp.com",
+    databaseURL: "https://lets-chat-web-app-2-31cbe-default-rtdb.firebaseio.com",
+    projectId: "lets-chat-web-app-2-31cbe",
+    storageBucket: "lets-chat-web-app-2-31cbe.appspot.com",
+    messagingSenderId: "988319376940",
+    appId: "1:988319376940:web:f9043f1b2352e48c165389"
+  };
+  
+  // Initialize Firebase
+   firebase.initializeApp(firebaseConfig);
